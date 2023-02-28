0723b304fb38ba495cbac42c21562239961e82b8
RaquelJanine <58750620+R1310@users.noreply.github.com>
2023-02-27 22:53:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./Componentes/Header"
+import Card from "./Componentes/Card"
+import"./style.css"
+import Footer from "./Componentes/Footer"
+
+function App() {
+  return(
+    <div className="container" >
+      <Header />
+      <main>
+        <Card title="Comida Japonesa"
+        img= "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2021/10/01/1122754635-comidajaponesaaprenda6receitasfaceis.jpg"/>
+
+        <Card title="Comida Japonesa"
+        img= "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2021/10/01/1122754635-comidajaponesaaprenda6receitasfaceis.jpg"/>
+
+        <Card title="Comida Japonesa"
+        img= "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2021/10/01/1122754635-comidajaponesaaprenda6receitasfaceis.jpg"/>
+
+        <Card title="Comida Japonesa"
+        img= "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2021/10/01/1122754635-comidajaponesaaprenda6receitasfaceis.jpg"/>
+
+        <Card title="Comida Japonesa"
+        img= "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2021/10/01/1122754635-comidajaponesaaprenda6receitasfaceis.jpg"/>
+
+
+      </main>
+      <Footer />
+
+    </div>
+  )
+  
+}
+export default App
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+return(
+    <header className="header">
+        <img></img>
+        <ul className="lista">
+        <li>Home</li>
+        <li>Quem somos</li>
+        <li>Contato</li>
+      </ul>
+    </header>
+);    
+
+}
+export default Header;
